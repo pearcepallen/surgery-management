5f2511044291d7917bd3975fa02c96f4bfefe307
Pearce Allen <pearce.p.allen@gmail.com>
2021-05-09 22:04:08 -0500

    Added model serializers

---
+++ backend/base/serializers.py
@@ -0,0 +1,58 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User, Group
+# from rest_framework_simplejwt.tokens import RefreshToken
+from . models import *
+
+
+class GroupSerializer(serializers.ModelSerializer):    
+    class Meta:
+        model = Group
+        fields = ['name',]
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+    id = serializers.SerializerMethodField(read_only=True)
+    groups = GroupSerializer(many=True)
+    class Meta:
+        model = User
+        # fields = '__all__'
+        fields = ['id', 'username', 'email', 'groups']
+
+    def get_id(self, obj):
+        return obj.id
+
+
+
+    class StaffSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Staff
+            fields = '__all__'
+
+
+
+    class PatientSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Patient
+            fields = '__all__'
+
+
+
+    class RoomSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Room
+            fields = '__all__'
+        
+
+
+    class SurgerySerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Surgery
+            fields = '__all__'
+    
+
+
+    class DoctorSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Doctor
+            fields = '__all__'
