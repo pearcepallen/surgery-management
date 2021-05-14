d4dbda5639e183af11e7fcf46ea93ddebd74dd4d
Pearce Allen <pearce.p.allen@gmail.com>
2021-05-14 18:38:43 -0500

    Added user role permissions to room endpoints

--- backend/base/views/room_views.py
+++ backend/base/views/room_views.py
@@ -12,19 +12,26 @@ from rest_framework import status
 @api_view(['GET', 'POST'])
 @permission_classes([IsAuthenticated])
 def getRooms(request):
-    
+    user = request.user
+
     if request.method == 'POST':
-        data = request.data
-        room = Room.objects.create(
-            roomName = data['roomName']
-        )
-        
-        serializer = RoomSerializer(room, many=False)
-        return Response({
-                'message':'Successfully created room',
-                'code': 0,
-                'data':serializer.data
-                })   
+        if user.groups.filter(name='admin'):
+            data = request.data
+            room = Room.objects.create(
+                roomName = data['roomName']
+            )
+            
+            serializer = RoomSerializer(room, many=False)
+            return Response({
+                    'message':'Successfully created room',
+                    'code': 0,
+                    'data':serializer.data
+                    })   
+        else:
+            return Response({
+                    'message':'Not allowed to create a room',
+                    'code' : 1,
+                    })
     
     rooms = Room.objects.all()
     serializer = RoomSerializer(rooms, many=True)
@@ -39,26 +46,39 @@ def getRooms(request):
 @permission_classes([IsAuthenticated])
 def getRoom(request, pk):
     try:
+        user = request.user
         room = Room.objects.get(id=pk)
 
         if request.method == 'PUT':
-            data = request.data
-            room.roomName=data['roomName']
-            room.save()
+            if user.groups.filter(name='admin'):
+                data = request.data
+                room.roomName=data['roomName']
+                room.save()
 
-            serializer = RoomSerializer(room, many=False)
-            return Response({
-                'message':'Successfully updated room',
-                'code' : 0,
-                'data' : serializer.data
-                })
+                serializer = RoomSerializer(room, many=False)
+                return Response({
+                    'message':'Successfully updated room',
+                    'code' : 0,
+                    'data' : serializer.data
+                    })
+            else:
+                return Response({
+                        'message':'Not allowed to update a room',
+                        'code' : 1,
+                        })
 
         if request.method == 'DELETE':
-            room.delete()
-            return Response({
-                    'message':'Room deleted',
-                    'code' : 0
-                    })
+            if user.groups.filter(name='admin'):
+                room.delete()
+                return Response({
+                        'message':'Room deleted',
+                        'code' : 0
+                        })
+            else:
+                return Response({
+                        'message':'Not allowed to delete a room',
+                        'code' : 1,
+                        })
 
         serializer = RoomSerializer(room, many=False)
         return Response({
